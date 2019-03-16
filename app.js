582aec3cc6a99548889b2060512826ccfd664f9d
FarooqQaisrani <farooqqaisrani@outlook.com>
2019-03-16 17:06:52 +0400

    section 5 done

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
-const http = require('http');
+const path = require('path');
 
-const routes = require('./routes');
+const express = require('express');
 
-const server = http.createServer(routes.handler);
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin',adminData.routes);
+app.use(shopRoutes);
+
+app.use( (req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
 
 
-server.listen(3000);
+
+
+// const server = http.createServer(app);
+
+app.listen(3001);
---
+++ routes/shop.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+const express = require('express');
+
+const rootDir = require('../util/path');
+
+const adminData = require('./admin');
+
+const router = express.Router();
+
+
+router.get('/', (req, res, next) => {
+  console.log(adminData.products);
+  // console.log('Start middleware!');
+  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+});
+
+module.exports = router;
+
