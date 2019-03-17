fec7b29375adae7c28d1b5cb9dca61cf6b2701ef
FarooqQaisrani <farooqqaisrani@outlook.com>
2019-03-17 15:08:16 +0400

    section 6 lec 89 done

--- routes/admin.js
+++ routes/admin.js
@@ -8,14 +8,20 @@ const router = express.Router();
 
 const products = [];
 
+// /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-  // console.log('Product middleware!');
-  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+  res.render('add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true
+  });
 });
 
+// /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
-  // console.log(req.body);
-  products.push({title: req.body.title});
+  products.push({ title: req.body.title });
   res.redirect('/');
 });
 
