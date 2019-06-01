6ab4783a5b7787170413544ace06f67b4d1bb452
FarooqQaisrani <farooqqaisrani@outlook.com>
2019-06-01 17:55:48 +0400

    section 11 lec 154 done

---
+++ util/database.js
@@ -0,0 +1,21 @@
+// const mysql = require('mysql2');
+//
+// const pool = mysql.createPool({
+//     host: 'localhost',
+//     port: "8083",
+//     user: 'root',
+//     database: 'node-complete',
+//     password: 'root'
+// });
+//
+// module.exports = pool.promise();
+
+const Sequelize = require('sequelize');
+
+const sequqlize = new Sequelize('node-complete', 'root', 'root', {
+    dialect: 'mysql',
+    host: 'localhost',
+    port: '8083'
+});
+
+module.exports = sequqlize;
