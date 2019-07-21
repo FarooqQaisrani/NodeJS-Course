75baed012a7d6dfcc7fe0bb6c1d1a140b16fc077
FarooqQaisrani <farooqqaisrani@outlook.com>
2019-07-22 00:17:31 +0400

    section 16 done
    Signed-off-by: FarooqQaisrani <farooqqaisrani@outlook.com>

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,17 @@
 const bcrypt = require("bcryptjs");
+const nodemailer = require("nodemailer");
+const sendgridTransport = require("nodemailer-sendgrid-transport");
 
 const User = require("../models/user");
+
+const transporter = nodemailer.createTransport(
+  sendgridTransport({
+    auth: {
+      api_key:
+        "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"
+    }
+  })
+);
 
 exports.getLogin = (req, res, next) => {
   let Cookies = req.get("Cookie").split(";");
@@ -145,6 +156,15 @@ exports.postSignup = (req, res, next) => {
           })
           .then(result => {
             res.redirect("/login");
+
+            // return transporter
+            //   .sendMail({
+            //     to: "farooqqaisrani@outlook.com",
+            //     from: "shop@node-complete.com",
+            //     subject: "Signup succeeded!",
+            //     html: `<h1>You successfully signed up!</h1>`
+            //   })
+            //   .catch(err => console.log(err));
           });
       }
     })
