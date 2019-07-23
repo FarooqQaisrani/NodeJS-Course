a293ff128758f1d34785c641c588898e9ab8c38a
FarooqQaisrani <farooqqaisrani@outlook.com>
2019-07-23 22:50:25 +0400

    section 16 done
    Signed-off-by: FarooqQaisrani <farooqqaisrani@outlook.com>

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,8 @@ const bcrypt = require("bcryptjs");
 const nodemailer = require("nodemailer");
 const mailtrap = require("../config/apiKeys.js");
 
+const { validationResult } = require("express-validator");
+
 const User = require("../models/user");
 
 const transporter = nodemailer.createTransport(mailtrap);
@@ -116,6 +118,15 @@ exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   const confirmPassword = req.body.confirmPassword;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    console.log(errors);
+    return res.status(422).render("auth/signup", {
+      path: "/signup",
+      pageTitle: "Signup",
+      errorMessage: errors.array()[0].msg
+    });
+  }
 
   User.findOne({
     where: {
